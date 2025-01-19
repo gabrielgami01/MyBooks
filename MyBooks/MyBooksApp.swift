a72ac4ae8e202e610a44af0a309ffc1b0f99aa7d
Gabriel García Millán <gabriel.garciamillan01@gmail.com>
2025-01-19 12:27:52 +0100

    Initial Commit

---
+++ MyBooks/MyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by Gabriel Garcia Millan on 19/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyBooksTests/MyBooksTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksTests.swift
+//  MyBooksTests
+//
+//  Created by Gabriel Garcia Millan on 19/1/25.
+//
+
+import Testing
+@testable import MyBooks
+
+struct MyBooksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
