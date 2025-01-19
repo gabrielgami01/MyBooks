5851cd6b1a74459513ded049e6f221c652e64dce
Gabriel García Millán <gabriel.garciamillan01@gmail.com>
2025-01-19 13:28:16 +0100

    Setup ModelContainer

--- MyBooks/System/MyBooksApp.swift
+++ MyBooks/System/MyBooksApp.swift
@@ -1,14 +1,22 @@
-//
-//  MyBooksApp.swift
-//  MyBooks
-//
-//  Created by Gabriel Garcia Millan on 19/1/25.
-//
-
 import SwiftUI
+import SwiftData
 
 @main
 struct MyBooksApp: App {
+    let container: ModelContainer
+    
+    init() {
+        let schema = Schema([Book.self])
+        let config = ModelConfiguration("MyBooks", schema: schema)
+        do {
+            container = try ModelContainer(for: schema, configurations: config)
+        } catch {
+            fatalError("Could not configure the container")
+        }
+        
+        print(URL.applicationSupportDirectory.path(percentEncoded: false))
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
