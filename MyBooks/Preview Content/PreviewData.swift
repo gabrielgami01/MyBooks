ba24bf522b6ee02408042dbc70e46ccbc26d8982
Gabriel García Millán <gabriel.garciamillan01@gmail.com>
2025-01-23 13:04:19 +0100

    Added Genre preview data

--- MyBooks/Preview Content/PreviewData.swift
+++ MyBooks/Preview Content/PreviewData.swift
@@ -53,3 +53,14 @@ extension Book {
         ]
     }
 }
+
+extension Genre {
+    static var sampleGenres: [Genre] {
+        [
+            Genre(name: "Fiction", color: "00FF00"),
+            Genre(name: "Non Fiction", color: "0000FF"),
+            Genre(name: "Romance", color: "FF0000"),
+            Genre(name: "Thriller", color: "000000")
+        ]
+    }
+}
